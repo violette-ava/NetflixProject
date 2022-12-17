72aac422700307b1a20ca6edeb22618a50d090f1
Ava Violette <avakviolette@yahoo.com>
2022-12-17 12:35:31 -0600

    Help Me

--- Netflix.java
+++ Netflix.java
@@ -1,26 +1,21 @@
 public class Netflix {
-    private static ArrayList<TVShow> tvshows = new ArrayList<>();
-    private static ArrayList<Movie> movies = new ArrayList<>();
-    import java.util.ArrayList;
-import java.util.Scanner; 
+
+    import java.util.Scanner;
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args){
-
         int choice = 1;
-        while(choice > 0){
-            System.out.println("Welcome To Netflix");
+        while (choice > 0){
+            System.out.println("Welcome to Netflix");
             System.out.println();
-            System.out.println("What would you like to watch? ");
+            System.out.println("What would you like to watch?");
+            System.out.println();
             System.out.println("1) TV Show");
             System.out.println("2) Movie");
-            System.out.println();
-            System.out.println("Choice: ");
-            choice = sc.nextInt;
+            choice = sc.nextInt();
             sc.nextLine; 
-
         }
-
        
-    }
     
+}
 }
